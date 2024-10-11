2a780780122989db4c319c2e550dee68472c2b34
Koutaro Mukai <mukai.k1011k@outlook.jp>
2024-10-11 14:04:25 +0900

    Enable --version option in CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
 import { promises as fs } from "node:fs";
+import path from 'node:path';
+import url from 'node:url';
 
 import { Command } from "commander";
 
 import { stripScripts } from "./index.js";
 
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const packageJsonPath = path.join(__dirname, "..", "package.json");
+const packageJson = JSON.parse(await fs.readFile(packageJsonPath, "utf-8"));
+
 new Command()
+    .version(packageJson.version)
     .argument("<input>")
     .argument("[output]")
     .action(async (input: string, output: string) => {
